fdea029f1c0d3518793af91483322126a8950f39
Ofridani <104002869+Ofridani@users.noreply.github.com>
2025-01-09 15:25:34 +0200

    Created grid coloring feature

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const container = document.querySelector(".gridContainer");
 for (let i = 0; i < 16*16; i++){
     const gridSquare = document.createElement("div");
     gridSquare.className = "gridSquare";
-    gridSquare.style.border = "2px solid black";
+    gridSquare.style.border = "1px solid black";
     gridSquare.style.height = `${100/16}%`;
     gridSquare.style.width = `${100/16}%`;
+    
     container.appendChild(gridSquare);
 }
+
+const onmouseover = (event) => {
+    if (!event.target.classList.contains("hovered")){
+        event.target.classList.add("hovered")
+    }
+}; 
+container.addEventListener("mouseover", onmouseover);

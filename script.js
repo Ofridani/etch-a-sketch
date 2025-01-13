dd4ff68a843a1f5c081943f4cb710e655e1863e0
Ofridani <104002869+Ofridani@users.noreply.github.com>
2025-01-13 14:47:07 +0200

    Functionalized code

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const container = document.querySelector(".gridContainer");
-for (let i = 0; i < 16*16; i++){
-    const gridSquare = document.createElement("div");
-    gridSquare.className = "gridSquare";
-    gridSquare.style.border = "1px solid black";
-    gridSquare.style.height = `${100/16}%`;
-    gridSquare.style.width = `${100/16}%`;
-    
-    container.appendChild(gridSquare);
-}
 
 const onmouseover = (event) => {
     if (!event.target.classList.contains("hovered")){
         event.target.classList.add("hovered")
     }
-}; 
-container.addEventListener("mouseover", onmouseover);
+}
+
+const createGrid = function(sideLength){
+    for (let i = 0; i < sideLength*sideLength; i++){
+        const gridSquare = document.createElement("div");
+        gridSquare.className = "gridSquare";
+        gridSquare.style.border = "1px solid black";
+        gridSquare.style.height = `${100/sideLength}%`;
+        gridSquare.style.width = `${100/sideLength}%`;
+        container.appendChild(gridSquare);
+    }
+    container.addEventListener("mouseover", onmouseover);
+}
+
+createGrid(16)
+
